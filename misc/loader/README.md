7344006c321af9d1f73e2f39305a9c262f5f8c19
xsling <me@xsl.sh>
2025-08-09 17:08:49 -0400

    Clean up

--- misc/loader/README.md
+++
@@ -1,8 +0,0 @@
-# Workload Loader
-
-This directory contains the abstraction layer that allows `flashinfer-bench` to support multiple kernel dataset formats seamlessly.
-
----
-
-#TODO
-Refactor, separate different different backends from abstraction
--- tools/leaderboard/leaderboard/models.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from leaderboard.utils.model_utils import list_available_models
-
-blueprint = Blueprint("models", __name__)
-
-@blueprint.route("/models")
-def list_models():
-    models = list_available_models()
-    return render_template("models.html", models=models)
