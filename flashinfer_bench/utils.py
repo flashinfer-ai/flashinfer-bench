eddede796e51c76d827d0651aa6346cc5cf0c555
Ubospica <ubospica@gmail.com>
2026-02-01 19:00:36 -0500

    update
    Signed-off-by: Ubospica <ubospica@gmail.com>

--- flashinfer_bench/utils.py
+++ flashinfer_bench/utils.py
@@ -164,12 +164,15 @@ def redirect_stdio_to_file(log_path: str) -> tuple[int, int]:
     Returns original stdout and stderr file descriptors for printing to terminal.
     """
     os.makedirs(os.path.dirname(log_path), exist_ok=True)
+    sys.stdout.flush()
+    sys.stderr.flush()
     original_stdout_fd = os.dup(1)
     original_stderr_fd = os.dup(2)
     fd = os.open(log_path, os.O_CREAT | os.O_WRONLY | os.O_TRUNC, 0o644)
     # Redirect stdout/stderr to log file
     os.dup2(fd, 1)  # stdout -> fd
     os.dup2(fd, 2)  # stderr -> fd
+    os.close(fd)
     sys.stdout = open(1, "w", encoding="utf-8", buffering=1, closefd=False)
     sys.stderr = open(2, "w", encoding="utf-8", buffering=1, closefd=False)
     return original_stdout_fd, original_stderr_fd
